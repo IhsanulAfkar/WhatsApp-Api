1c8eb1279531e19f70b2f8c5aa562422baa85b98
IhsanulAfkar <ihsanul2001@gmail.com>
2024-06-23 14:02:03 +0700

    update

--- src/utils/chatbot.ts
+++ src/utils/chatbot.ts
@@ -132,9 +132,10 @@ export const processChatbot = async (device: Device, intent: string, confidence:
             const product = await prisma.product.findFirst({
                 where: { name: productName, userId: user.pkId }
             })
+            console.log(product)
             if (!product)
                 return
-            if (product.amount == 0) {
+            if (product.amount === 0) {
                 responseMessage = `Mohon maaf, untuk ${product.name} sedang tidak ready`
             }
             const responseProduct = `${product.name}\n${formatToIDR(product.price)}\n${product.description || ""} \nReady ya`
