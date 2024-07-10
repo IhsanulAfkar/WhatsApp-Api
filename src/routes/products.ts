1f64bb7a608dc675a104565b13d57a4c091220b7
IhsanulAfkar <ihsanul2001@gmail.com>
2024-07-10 19:11:48 +0700

    add db visualization

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -2,8 +2,6 @@ import { Router } from "express"
 import * as controller from '../controllers/product'
 const router = Router()
 router.get('/', controller.getAllProducts)
-// router.delete('/', controller.deleteProducts)
-// router.post('/create', controller.createProduct)
 router.get('/:productId', controller.getProduct)
 router.put('/:productId/edit', controller.editProduct)
 router.patch('/:productId/amount', controller.changeAmount)
